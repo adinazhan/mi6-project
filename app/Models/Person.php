317d83da0d16a9e43a02b24d6414dff72ed2c5ab
adinazhan <a.zhantykeyeva@gmail.com>
2024-11-11 11:56:39 +0100

    person relations created

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Alias;
+use App\Models\Image;
+use App\Models\Status;
 
 class Person extends Model
 {
-    //
+    public function aliases()
+    {
+        return $this->hasMany(Alias::class);
+    }
+    public function images()
+    {
+        return $this->belongsTo(Image::class);
+    }
+    public function statuses()
+    {
+        return $this->belongsTo(Status::class);
+    }
 }
